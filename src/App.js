5c332ec56e11d78228312caa08b5f390a979d517
Georg Westner <georg.westner@googlemail.com>
2017-06-14 16:40:54 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
+import { Provider } from 'react-redux';
+
+import store from './store';
+
+import Layout from './components/Layout/Layout';
+
+import Home from './pages/Home/Home';
+import OttenList from './pages/OttenList/OttenList';
+import OttenDetail from './pages/OttenDetail/OttenDetail';
+
+import './App.css';
+
+const history = createBrowserHistory();
+
+class App extends Component {
+
+  render() {
+    return (
+      <Provider store={store}>
+        <Router history={history}>
+          <Layout>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/otten" component={OttenList} />
+            <Route exact path="/otte/:otteId" component={OttenDetail} />
+          </Layout>
+        </Router>
+      </Provider>
+    );
+  }
+
+}
+
+export default App;
