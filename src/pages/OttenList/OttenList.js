c3442077661f904d2251a698cd59fe94852f5dcb
Georg Westner <Georg Westner>
2017-06-18 11:34:31 +0200

    needs correct redux flow

--- src/pages/OttenList/OttenList.js
+++ src/pages/OttenList/OttenList.js
@@ -3,7 +3,22 @@ import { connect } from 'react-redux';
 
 import TableOfOtten from '../../components/TableOfOtten/TableOfOtten';
 
+import store from '../../store';
+
+
 class OttenList extends Component {
+
+  constructor(props){
+    super(props);
+
+    store.subscribe(() => {
+      // When state will be updated(in our case, when items will be fetched), we will update local component state and force component to rerender with new data.
+      console.log("changed state");
+      this.props.otten = store.getState().otten;
+
+        this.setState({})
+    });
+  }
 
   render() {
     const { otten } = this.props;
