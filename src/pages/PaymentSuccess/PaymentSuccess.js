79f596e8d80561dbd12975052a50418ad1f6441c
Michael Bredehorn <bredehornm@gmail.com>
2017-07-07 23:50:39 +0200

    cleanup

--- src/pages/PaymentSuccess/PaymentSuccess.js
+++ src/pages/PaymentSuccess/PaymentSuccess.js
@@ -32,31 +32,29 @@ class PaymentSuccess extends Component {
   }
 
   render() {
-    {
-      if (this.state.timerFinished === false) {
-        return (
-          <div>
-            <Button
-              className="button icon-left"
-              onClick={() => this.props.history.goBack()}>
-              Back
-            </Button>
-            <h1>Your Payment was successfull!</h1>
-            <h1>Thank you for choosing OttenUber</h1>
-            <h2>Your Otten will arrive in: </h2>
-            <h1>
-              {this.state.currentCount}
-            </h1>
-          </div>
-        )
-      }
-      else {
-        return (
-          <Alert bsStyle="success">
-            <strong>Viel Spass mit dem Otten!</strong>
-          </Alert>
-        )
-      }
+    if (this.state.timerFinished === false) {
+      return (
+        <div>
+          <Button
+            className="button icon-left"
+            onClick={() => this.props.history.goBack()}>
+            Back
+          </Button>
+          <h1>Your Payment was successfull!</h1>
+          <h1>Thank you for choosing OttenUber</h1>
+          <h2>Your Otten will arrive in: </h2>
+          <h1>
+            {this.state.currentCount}
+          </h1>
+        </div>
+      )
+    }
+    else {
+      return (
+        <Alert bsStyle="success">
+          <strong>Viel Spass mit dem Otten!</strong>
+        </Alert>
+      )
     }
   }
 }
